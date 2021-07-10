382b2acd22bbb676555997da03232821a5e83421
HARSHIT SHARMA <harsh007fc@gmail.com>
2021-07-10 18:10:24 +0530

    added upto firebase created

---
+++ src/Firebase.js
@@ -0,0 +1,25 @@
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/storage'
+import 'firebase/firestore'
+
+firebase.initializeApp(
+    {
+        apiKey: "AIzaSyBTB3eAxKeiEHJ6hY3RiSpK8Mh_x8tFvKg",
+        authDomain: "reels-42617.firebaseapp.com",
+        projectId: "reels-42617",
+        storageBucket: "reels-42617.appspot.com",
+        messagingSenderId: "135168045751",
+        appId: "1:135168045751:web:86e0719c452c53e3db9332"
+      }
+)
+
+
+export let auth = firebase.auth();
+let firestore = firebase.firestore();
+//saare firestore ka access ni dena hota bs jo use ayenge unka hi dena hota hai
+export let database = {
+    user:firestore.collection('user')
+}
+
+export default firebase;

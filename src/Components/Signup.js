948f8e5da92c7c5485a13852f4d2c1b2e0224f55
HARSHIT SHARMA <harsh007fc@gmail.com>
2021-07-11 16:56:44 +0530

    login added with pic

--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -1,8 +1,9 @@
 import React,{useContext,useEffect,useState} from 'react'
 import {AuthContext} from '../Context/AuthProvider'
-
+import {storage} from '../firebase'
 function Signup() {
     let [email,setMail] = useState('');
+    let [file,setFile] = useState(null);
     let [password,setPassword] = useState('');
     let [name,setName] = useState('');
     let [error,setError] = useState('');
@@ -17,10 +18,38 @@ function Signup() {
         setLoading(true);
         let res = await signup(email,password);
         let uid = res.user.uid;
+        let uploadTaskListner = storage.ref(`/users/${uid}/profileImage`).put(file);
+        // fn1-> uploading track
+        //fn2 -> error 
+        //fn 3 -> success
+        uploadTaskListner.on('state_changed',fn1,fn2,fn3);
+        function fn1(snapshot){
+             var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+             console.log('Upload is '+ progress+' % done' );
+        }
+
+        function fn2(error){
+            setError(error);
+            setTimeout(()=>{
+                setError('');
+            },2000);
+            setLoading(false);
+        }
+
+        async function fn3(){
+            let downloadUrl = await uploadTaskListner.snapshot.ref.getDownloadURL();
+            console.log(downloadUrl);
+        }
         console.log(uid);
         setLoading(false)
     }
 
+    let handleFileSubmit = (e) => {
+        let file = e.target.files[0];
+        if(file){
+            setFile(file);
+        }
+    }
     return (
         <div>
             <form onSubmit={handleSignup}>
@@ -35,6 +64,10 @@ function Signup() {
                 <div>
                     <label htmlFor="">Password</label>
                     <input type="password" value={password} onChange={(e) => { setPassword(e.target.value) }} />
+                </div>
+                <div>
+                    <label htmlFor="profile">Profile Image </label>
+                    <input type="file" accept='image/*' onChange={handleFileSubmit}  />
                 </div>
                 <button type='submit' disabled={loading}>Login</button>
             </form>

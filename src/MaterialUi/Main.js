5578fa7d7790f0839ace75eeca6e71634703a2b6
HARSHIT SHARMA <harsh007fc@gmail.com>
2021-07-11 20:59:35 +0530

    upto button

--- src/MaterialUi/Main.js
+++ src/MaterialUi/Main.js
@@ -1,10 +1,12 @@
 
 import React from 'react'
 import Navbar from './Navbar';
+import Buttons from './Buttons';
 function Main() {
     return (
         <div>
             <Navbar/>
+            <Buttons/>
         </div>
     )
 }
